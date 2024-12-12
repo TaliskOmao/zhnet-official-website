36c4f36efd550f92444b68f3e34e7dbecc229735
TALISK O. <i@eden-official.co.uk>
2024-12-12 09:24:57 +0200

    Update colo.vue

--- src/components/views/dashboard/colo.vue
+++ src/components/views/dashboard/colo.vue
@@ -10,7 +10,7 @@
         <div><h1 align="left">OMAO NETWORK offers colocation with immediate availability. We can have you setup OMAO ESTONIA EE1 the same day you order service.<br><br></h1></div>
         <div><h1 align="left">We offer plans that range from cabinet space for a single 1U server, a full cabinet, or enough cabinet space to house tens of thousands of servers.<br><br></h1></div>
         <div><h1 align="left">Within our facilities, OMAO can provide you with a connection to the Internet from 100Mbps Ethernet up to 10 Gigabit Ethernet from ELISA EE.<br><br></h1></div>
-        <font color="Green"><div><h1 align="left"><a href="https://portal.shinobu.is/" target="_blank">Open in Platform</a></h1></div></font><br>
+        <font color="Green"><div><h1 align="left"><a href="https://portal.omao.ee/" target="_blank">Open in Platform</a></h1></div></font><br>
         <div class="flex-33">
               <div class="text-center">
                 <header class="box-header">
